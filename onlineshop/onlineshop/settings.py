996d99b0d68d874589d1220e90f4928e8a83e8fa
YuTymchuk <yu.tymchuk@gmail.com>
2023-02-03 05:09:24 +0200

    Identification

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -124,3 +124,19 @@ STATICFILES_DIRS = ['onlineshop/static', ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'pryviteveryone@yahoo.com'
+# EMAIL_HOST_PASSWORD = 'C!02x)1U*eZf8VO+I'
+EMAIL_HOST_PASSWORD = 'uponrtjmhnbzjcop'
+EMAIL_USE_TLS = True
+# this is needed for yahoo
+DEFAULT_FROM_EMAIL = 'pryviteveryone@yahoo.com'
